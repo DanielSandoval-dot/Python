68ea8953c30dc01323198b0d900f4daabe049320
Daniel Sandoval <dgsandovalm01@gmail.com>
2023-08-17 22:18:15 +0200

    Added file: Turtle module.py

---
+++ Turtle module.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 13 10:42:17 2023
+
+@author: danie
+"""
+
+import turtle
+wn = turtle.Screen()
+tess= turtle.Turtle()
+tess.forward(50)
+turtle.done()
+turtle.bye()
